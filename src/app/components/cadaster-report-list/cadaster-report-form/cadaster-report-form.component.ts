989394ead6dbfe893b75d5e17cad4a0f3cdd75e8
Zhussipov Meirambek <make202006@gmail.com>
2021-12-29 10:42:00 +0600

    отчеты

---
+++ src/app/components/cadaster-report-list/cadaster-report-form/cadaster-report-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadaster-report-form',
+  templateUrl: './cadaster-report-form.component.html',
+  styleUrls: ['./cadaster-report-form.component.css']
+})
+export class CadasterReportFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
