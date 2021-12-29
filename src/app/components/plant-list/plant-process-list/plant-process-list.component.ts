fdd2f8c7f01ea42eef56fae9e36bcaf4d50ccd74
Zhussipov Meirambek <make202006@gmail.com>
2021-12-29 10:40:51 +0600

    процессы

---
+++ src/app/components/plant-list/plant-process-list/plant-process-list.component.ts
@@ -0,0 +1,221 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  AngularGridInstance,
+  Column,
+  Formatters,
+  GridOption,
+  OnEventArgs,
+} from 'angular-slickgrid';
+import { Dictionary } from 'src/app/models/dictionary.model';
+import { PlantProcessModel } from 'src/app/models/plant-process.model ';
+import { PlantService } from 'src/app/services/plant.service';
+import { PlantProcessService } from '../../../services/plant-process.service';
+import { ProcessFormComponent } from './process-form/process-form.component';
+@Component({
+  selector: 'app-plant-process-list',
+  templateUrl: './plant-process-list.component.html',
+  styleUrls: ['./plant-process-list.component.css'],
+})
+export class PlantProcessListComponent implements OnInit {
+  angularGrid!: AngularGridInstance;
+  columnDefinitions: Column[] = [];
+  gridOptions: GridOption = {};
+  dataset: PlantProcessModel[] = [];
+  plantId!: number;
+  processId!: number;
+  gridObj: any;
+  dataViewObj: any;
+  isActive = false;
+  submitted = false;
+  namePlant!:string
+  dicProcessList: Dictionary[] = [];
+  @ViewChild('content') content: any;
+  ref: any;
+  angularGridReady(angularGrid: AngularGridInstance) {
+    this.angularGrid = angularGrid;
+    this.gridObj = angularGrid.slickGrid;
+    this.dataViewObj = angularGrid.dataView;
+  }
+
+  constructor(
+    private modalService: NgbModal,
+    private plantProcessService: PlantProcessService,
+    private plantService: PlantService
+  ) {}
+  ngOnInit(): void {
+    this.plantService.plantIdRefreshList.subscribe((item: any) => {
+      this.plantId = item.id;
+      // this.namePlant = item.namePlant;
+       this.refreshList(this.plantId);
+    });
+
+    this.prepareGrid();
+  }
+
+  refreshList(id: number) {
+    this.plantProcessService
+      .getPlantProcessList(id)
+      .subscribe((data) => (this.dataset = data));
+  }
+  anyFunction(id: number) {
+    this.plantId = id;
+    this.refreshList(id);
+  }
+  openPlantProcessModal() {
+    this.ref = this.modalService.open(ProcessFormComponent, { size: 'xl' });
+    this.addProcess();
+    this.updateProcess();
+  }
+  addProcess() {
+    this.ref.componentInstance.addProcess.subscribe((data: any) => {
+      this.plantProcessService
+        .addPlantProcess({ id: 0, plantId: this.plantId, ...data })
+        .subscribe(() => this.refreshList(this.plantId));
+    });
+  }
+  updateProcess() {
+    this.ref.componentInstance.updateProcess.subscribe((data: any) => {
+      this.plantProcessService
+        .updatePlantProcess({
+          id: this.processId,
+          plantId: this.plantId,
+          ...data,
+        })
+        .subscribe(() => this.refreshList(this.plantId));
+    });
+  }
+
+  prepareGrid() {
+    this.columnDefinitions = [
+      {
+        id: 'processName',
+        name: 'Наименование производственного процесса',
+        field: 'processName',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'materialNames',
+        name: 'Наименование топлива или сырья',
+        field: 'materialNames',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'oddsLevel',
+        name: 'Уровень коэффициентов выбросов парниковых газов',
+        field: 'oddsLevel',
+        filterable: true,
+        sortable: true,
+      },
+
+      {
+        id: 'amountConsumed',
+        name: 'Источник данных о количестве потребляемого топлива или сырья',
+        field: 'amountConsumed',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'calculatingCalorific',
+        name: 'низшей теплоты сгорания',
+        columnGroup: 'Источник данных для расчета коэффициента ',
+        field: 'calculatingCalorific',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'calculatingConversion',
+        name: 'преобразования (в случае промышленных процессов) ',
+        columnGroup: 'Источник данных для расчета коэффициента ',
+        field: 'calculatingConversion',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'calculatingCarbon',
+        name: 'содержания углерода ',
+        columnGroup: 'Источник данных для расчета коэффициента ',
+        field: 'calculatingCarbon',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'edit',
+        field: 'id',
+        excludeFromColumnPicker: true,
+        excludeFromGridMenu: true,
+        excludeFromHeaderMenu: true,
+        formatter: Formatters.editIcon,
+        minWidth: 30,
+        maxWidth: 30,
+        onCellClick: (e: Event, args: OnEventArgs) => {
+          this.processId = args.dataContext.id;
+          this.openPlantProcessModal();
+          this.ref.componentInstance.editForm(this.processId);
+        },
+      },
+      {
+        id: 'delete',
+        field: 'id',
+        excludeFromColumnPicker: true,
+        excludeFromGridMenu: true,
+        excludeFromHeaderMenu: true,
+        formatter: Formatters.deleteIcon,
+        minWidth: 30,
+        maxWidth: 30,
+        onCellClick: (e: Event, args: OnEventArgs) => {
+          const id = args.dataContext.id;
+          if (confirm('Уверены ли вы?')) {
+            this.plantProcessService
+              .deletePlantProcess(id)
+              .subscribe(() => this.refreshList(this.plantId));
+          }
+        },
+      },
+    ];
+
+    this.gridOptions = {
+      autoResize: {
+        container: '#demo-container',
+      },
+      enableCellNavigation: true,
+      createPreHeaderPanel: true,
+      showPreHeaderPanel: true,
+      preHeaderPanelHeight: 25,
+      explicitInitialization: true,
+      enableAutoSizeColumns: true,
+      enableAutoResize: true,
+      gridWidth: '100%',
+      enableFiltering: true,
+      enableSorting: true,
+      editable: true,
+      autoEdit: true,
+      autoCommitEdit: true,
+      enableTranslate: true,
+      enableColumnReorder: false,
+      enableColumnPicker: false,
+        columnPicker: {
+        hideForceFitButton: true,
+      },
+      headerMenu: {
+        hideFreezeColumnsCommand: false,
+      },
+      exportOptions: {
+        // set at the grid option level, meaning all column will evaluate the Formatter (when it has a Formatter defined)
+        exportWithFormatter: true,
+        sanitizeDataExport: true,
+      },
+      gridMenu: {
+        hideExportTextDelimitedCommand: false, // true by default, so if you want it, you will need to disable the flag
+      },
+      enableExcelExport: true,
+      checkboxSelector: {
+        // you can toggle these 2 properties to show the "select all" checkbox in different location
+        hideInFilterHeaderRow: false,
+        hideInColumnTitleRow: true,
+      },
+    };
+  }
+}
---
+++ src/app/components/plant-list/plant-process-list/process-form/process-form.component.ts
@@ -0,0 +1,122 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DicFormComponent } from 'src/app/components/dic-form/dic-form.component';
+import { Dictionary } from 'src/app/models/dictionary.model';
+import { DicMaterialService } from 'src/app/services/dic-materials.service';
+import { DicProcessService } from 'src/app/services/dic-process.service';
+import { PlantProcessService } from 'src/app/services/plant-process.service';
+
+@Component({
+  selector: 'app-process-form',
+  templateUrl: './process-form.component.html',
+  styleUrls: ['./process-form.component.css'],
+})
+export class ProcessFormComponent implements OnInit {
+  form: FormGroup;
+  isActive = false;
+  submitted = false;
+  dicProcessList: Dictionary[] = [];
+  dicMaterialsList: Dictionary[] = [];
+  ref: any;
+  @Output() addProcess: EventEmitter<any> = new EventEmitter();
+  @Output() updateProcess: EventEmitter<any> = new EventEmitter();
+  constructor(
+    private modalService: NgbModal,
+    private plantProcessService: PlantProcessService,
+    private fb: FormBuilder,
+    private dicMaterialService: DicMaterialService,
+    private dicProcessService: DicProcessService
+  ) {
+    this.form = this.fb.group({
+      dicProcessId: new FormControl('', Validators.required),
+      materials: new FormControl('', Validators.required),
+      oddsLevel: new FormControl('', Validators.required),
+      amountConsumed: new FormControl(),
+      calculatingCalorific: new FormControl(),
+      calculatingConversion: new FormControl(),
+      calculatingCarbon: new FormControl(),
+    });
+  }
+
+  ngOnInit(): void {
+    this.getDicProcess();
+    this.getDicMaterial();
+  }
+
+  openPlantProcessModal(name: string) {
+    this.ref = this.modalService.open(DicFormComponent, {
+      size: 'md',
+    });
+    if (name === 'dicMaterial') {
+      this.ref.componentInstance.dicTitle = 'Добавить материалы';
+      this.ref.componentInstance.dicLabel = 'Материал';
+      this.dicMaterialAdded();
+    } else if (name === 'dicProcess') {
+      this.ref.componentInstance.dicTitle = 'Добавить Процессы';
+      this.ref.componentInstance.dicLabel = 'Процесс';
+      this.dicProccessAdded();
+    }
+  }
+
+  dicMaterialAdded() {
+    this.ref.componentInstance.dicAdded.subscribe((data: Dictionary) => {
+      this.dicMaterialService.addDicMaterial(data).subscribe((res) => {
+        this.getDicMaterial();
+        this.form.controls['materials'].setValue([res.id]);
+      });
+    });
+  }
+  dicProccessAdded() {
+    this.ref.componentInstance.dicAdded.subscribe((data: Dictionary) => {
+      this.dicProcessService.addDicProcess(data).subscribe((res) => {
+        this.getDicProcess();
+        this.form.controls['dicProcessId'].setValue(res.id);
+      });
+    });
+  }
+
+  getDicProcess() {
+    this.dicProcessService.getDicProcess().subscribe((res) => {
+      this.dicProcessList = res;
+    });
+  }
+  getDicMaterial() {
+    this.dicMaterialService.getDicMaterial().subscribe((res) => {
+      this.dicMaterialsList = res;
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    const data = { ...this.form.value };
+    !this.isActive ? this.addProcess.emit(data) : this.updateProcess.emit(data);
+    this.hidePlantProcessModal();
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  hidePlantProcessModal() {
+    this.modalService.dismissAll();
+    this.form.reset();
+    this.isActive = false;
+    this.submitted = false;
+  }
+  editForm(id: number) {
+    this.isActive = true;
+    this.plantProcessService.getPlantProcessById(id).subscribe((data) => {
+      this.form.patchValue(data);
+    });
+  }
+}
