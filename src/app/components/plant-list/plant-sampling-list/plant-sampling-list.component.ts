8265f5e4ea0eea0735b967a45773ca609c1362b9
Zhussipov Meirambek <make202006@gmail.com>
2021-12-29 10:40:26 +0600

    отбор проб и форма

---
+++ src/app/components/plant-list/plant-sampling-list/plant-sampling-list.component.ts
@@ -0,0 +1,216 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  AngularGridInstance,
+  Column,
+  Formatters,
+  GridOption,
+  Observable,
+  OnEventArgs,
+} from 'angular-slickgrid';
+import { PlantSamplingModel } from 'src/app/models/plant-sampling.model';
+import { PlantService } from 'src/app/services/plant.service';
+import { PlantSamplingService } from '../../../services/plant-sampling.service';
+import { SamplingFormComponent } from './sampling-form/sampling-form.component';
+@Component({
+  selector: 'app-plant-sampling-list',
+  templateUrl: './plant-sampling-list.component.html',
+  styleUrls: ['./plant-sampling-list.component.css'],
+})
+export class PlantSamplingListComponent implements OnInit {
+  angularGrid!: AngularGridInstance;
+  columnDefinitions: Column[] = [];
+  gridOptions: GridOption = {};
+  dataset: PlantSamplingModel[] = [];
+  plantId!: number;
+  samplingId!: number;
+  gridObj: any;
+  dataViewObj: any;
+  ref: any;
+  namePlant!: string;
+  angularGridReady(angularGrid: AngularGridInstance) {
+    this.angularGrid = angularGrid;
+    this.gridObj = angularGrid.slickGrid;
+    this.dataViewObj = angularGrid.dataView;
+  }
+  constructor(
+    private modalService: NgbModal,
+    private plantService: PlantService,
+    private samplingService: PlantSamplingService
+  ) {}
+  ngOnInit(): void {
+    this.prepareGrid();
+    this.plantService.plantIdRefreshList.subscribe((item: any) => {
+      this.plantId = item.id;
+      // this.namePlant = item.namePlant;
+      this.refreshList(this.plantId);
+    });
+  }
+
+  anyFunction(id: number) {
+    this.plantId = id;
+    this.refreshList(id);
+  }
+
+  refreshList(id: number) {
+    this.samplingService.getPlantSamplingList(id).subscribe((data) => {
+      this.dataset = data;
+    });
+  }
+  openSamplingModal() {
+    this.ref = this.modalService.open(SamplingFormComponent, { size: 'xl' });
+    this.onSamplingAdded();
+    this.onSamplingUpdated();
+  }
+  onSamplingAdded() {
+    this.ref.componentInstance.onSamplingAdded.subscribe(
+      (data: PlantSamplingModel) => {
+        const newData = { id: 0, plantId: this.plantId, ...data };
+        this.samplingService.addPlantSampling(newData).subscribe((data) => {
+          this.refreshList(this.plantId);
+          console.log(data);
+        });
+      }
+    );
+  }
+
+  onSamplingUpdated() {
+    this.ref.componentInstance.onSamplingUpdated.subscribe(
+      (data: PlantSamplingModel) => {
+        const newData = {
+          id: this.samplingId,
+          plantId: this.plantId,
+          ...data,
+        };
+        this.samplingService.updatePlantSampling(newData).subscribe((data) => {
+          this.refreshList(this.plantId);
+        });
+      }
+    );
+  }
+
+  prepareGrid() {
+    this.columnDefinitions = [
+      {
+        id: 'nameSampling',
+        name: 'Точка отбора проб',
+        field: 'nameSampling',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'materialNames',
+        name: 'Вид топлива, сырья или продукции',
+        field: 'materialNames',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'param',
+        name: 'Параметр отбора проб',
+        field: 'param',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'methodSampling',
+        name: 'Метод отбора проб (краткое описание)',
+        field: 'methodSampling',
+        filterable: true,
+        sortable: true,
+      },
+
+      {
+        id: 'frequencySampling',
+        name: 'Периодичность отбора проб',
+        field: 'frequencySampling',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'periodTransmission',
+        name: 'Периодичность передачи данных для расчетов выбросов парниковых газов',
+        field: 'periodTransmission',
+        filterable: true,
+        sortable: true,
+      },
+
+      {
+        id: 'edit',
+        field: 'id',
+        excludeFromColumnPicker: true,
+        excludeFromGridMenu: true,
+        excludeFromHeaderMenu: true,
+        formatter: Formatters.editIcon,
+        minWidth: 30,
+        maxWidth: 30,
+        onCellClick: (e: Event, args: OnEventArgs) => {
+          this.samplingId = args.dataContext.id;
+          this.openSamplingModal();
+          this.ref.componentInstance.editForm(this.samplingId);
+        },
+      },
+      {
+        id: 'delete',
+        field: 'id',
+        excludeFromColumnPicker: true,
+        excludeFromGridMenu: true,
+        excludeFromHeaderMenu: true,
+        formatter: Formatters.deleteIcon,
+        minWidth: 30,
+        maxWidth: 30,
+        onCellClick: (e: Event, args: OnEventArgs) => {
+          const id = args.dataContext.id;
+          if (confirm('Уверены ли вы?')) {
+            this.samplingService.deletePlantSampling(id).subscribe((data) => {
+              this.refreshList(this.plantId);
+            });
+          }
+        },
+      },
+    ];
+
+    this.gridOptions = {
+      autoResize: {
+        container: '#demo-container',
+      },
+      enableAutoSizeColumns: true,
+      enableAutoResize: true,
+      gridWidth: '100%',
+      enableFiltering: true,
+      enableSorting: true,
+      enableCellNavigation: true,
+      editable: true,
+      autoEdit: true,
+      autoCommitEdit: true,
+      createPreHeaderPanel: true,
+      showPreHeaderPanel: false,
+      preHeaderPanelHeight: 23,
+      explicitInitialization: true,
+      enableTranslate: true,
+      enableColumnReorder: false,
+      enableColumnPicker: false,
+      enableRowSelection: true,
+      columnPicker: {
+        hideForceFitButton: true,
+      },
+      headerMenu: {
+        hideFreezeColumnsCommand: false,
+      },
+      exportOptions: {
+        // set at the grid option level, meaning all column will evaluate the Formatter (when it has a Formatter defined)
+        exportWithFormatter: true,
+        sanitizeDataExport: true,
+      },
+      gridMenu: {
+        hideExportTextDelimitedCommand: false, // true by default, so if you want it, you will need to disable the flag
+      },
+      enableExcelExport: true,
+      checkboxSelector: {
+        // you can toggle these 2 properties to show the "select all" checkbox in different location
+        hideInFilterHeaderRow: false,
+        hideInColumnTitleRow: true,
+      },
+    };
+  }
+}
---
+++ src/app/components/plant-list/plant-sampling-list/sampling-form/sampling-form.component.ts
@@ -0,0 +1,102 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DicFormComponent } from 'src/app/components/dic-form/dic-form.component';
+import { Dictionary } from 'src/app/models/dictionary.model';
+import { PlantSamplingModel } from 'src/app/models/plant-sampling.model';
+import { DicMaterialService } from 'src/app/services/dic-materials.service';
+import { PlantSamplingService } from 'src/app/services/plant-sampling.service';
+@Component({
+  selector: 'app-sampling-form',
+  templateUrl: './sampling-form.component.html',
+  styleUrls: ['./sampling-form.component.css'],
+})
+export class SamplingFormComponent implements OnInit {
+  form: FormGroup;
+  isActive = false;
+  submitted?: boolean;
+  dicMaterialsList: Dictionary[] = [];
+  ref: any;
+
+  @Output() onSamplingAdded: EventEmitter<PlantSamplingModel> = new EventEmitter();
+  @Output() onSamplingUpdated: EventEmitter<PlantSamplingModel> = new EventEmitter();
+  constructor(
+    private fb: FormBuilder,
+    private samplingService: PlantSamplingService,
+    private activeModal: NgbActiveModal,
+    private dicMaterialService: DicMaterialService,
+    private modalService: NgbModal
+  ) {
+    this.form = this.fb.group({
+      nameSampling: new FormControl('', Validators.required),
+      materials: new FormControl('', Validators.required),
+      param: new FormControl('', Validators.required),
+      methodSampling: new FormControl('', Validators.required),
+      frequencySampling: new FormControl('', Validators.required),
+      periodTransmission: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    this.getDicMaterial();
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+  editForm(id: any) {
+    this.isActive = true;
+    this.samplingService.getPlantSamplingById(id).subscribe((data) => {
+      this.form.patchValue(data);
+    });
+  }
+  openDicFormModal(name: string) {
+    this.ref = this.modalService.open(DicFormComponent, {
+      size: 'md',
+    });
+    if (name === 'dicMaterial') {
+      this.ref.componentInstance.dicTitle = 'Добавить материалы';
+      this.ref.componentInstance.dicLabel = 'Материал';
+      this.dicMaterialAdded();
+    }
+  }
+
+  dicMaterialAdded() {
+    this.ref.componentInstance.dicAdded.subscribe((data: Dictionary) => {
+      this.dicMaterialService.addDicMaterial(data).subscribe((res) => {
+        this.getDicMaterial();
+        this.form.controls['materials'].setValue([res.id]);
+      });
+    });
+  }
+
+  getDicMaterial() {
+    this.dicMaterialService.getDicMaterial().subscribe((res) => {
+      this.dicMaterialsList = res;
+      console.log(res);
+    });
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    const data = { ...this.form.value };
+    !this.isActive
+      ? this.onSamplingAdded.emit(data)
+      : this.onSamplingUpdated.emit(data);
+    this.hideSamplingModal();
+  }
+
+  hideSamplingModal() {
+    this.isActive = false;
+    this.activeModal.close();
+    this.form.reset();
+    this.submitted = false;
+  }
+}
