db12def8044a05c113dc5a820e30c65d96aeb56a
Zhussipov Meirambek <make202006@gmail.com>
2022-02-03 15:38:14 +0600

    added @ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cadaster';
+  constructor(public translate: TranslateService) {
+    translate.setDefaultLang('ru');
+    translate.use('ru');
+    translate.addLangs(['ru', 'en', 'kz']);
+  }
 }
