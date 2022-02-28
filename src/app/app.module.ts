09c1342de5709050e35d422b0bd24606a8a404b9
Zhussipov Meirambek <make202006@gmail.com>
2022-02-28 13:37:57 +0600

    add grid global options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,17 @@ import {
   TranslateLoader,
   TranslateService,
 } from '@ngx-translate/core'
-import { AngularSlickgridModule } from 'angular-slickgrid'
+import {
+  AngularSlickgridModule,
+  Column,
+  DelimiterType,
+  EventNamingStyle,
+  FileType,
+  Filters,
+  OperatorType,
+  RowDetailView,
+  TreeDataOption,
+} from 'angular-slickgrid'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { PlantListComponent } from './components/plant-list/plant-list.component'
@@ -52,9 +62,10 @@ import {
   MAT_FORM_FIELD_DEFAULT_OPTIONS,
 } from '@angular/material/form-field'
 import { CadasterReportCheckComponent } from './components/cadaster-report-check/cadaster-report-check.component'
- import { CdrReportCheckListsComponent } from './components/cdr-report-check-list/cdr-report-check-list.component';
-import { PlantPlannedChangesComponent } from './components/plant-list/plant-planned-changes/plant-planned-changes.component';
-import { PlannedChangesFormComponent } from './components/plant-list/plant-planned-changes/planned-changes-form/planned-changes-form.component';
+import { CdrReportCheckListsComponent } from './components/cdr-report-check-list/cdr-report-check-list.component'
+import { PlantPlannedChangesComponent } from './components/plant-list/plant-planned-changes/plant-planned-changes.component'
+import { PlannedChangesFormComponent } from './components/plant-list/plant-planned-changes/planned-changes-form/planned-changes-form.component'
+import { ExcelExportService } from '@slickgrid-universal/excel-export'
 
 const appearance: MatFormFieldDefaultOptions = {
   appearance: 'outline',
@@ -123,15 +134,245 @@ export function appInitializerFactory(
     CommentHistoryComponent,
     CustomInputEditorComponent,
     CadasterReportCheckComponent,
-     CdrReportCheckListsComponent,
-     PlantPlannedChangesComponent,
-     PlannedChangesFormComponent,
-   ],
+    CdrReportCheckListsComponent,
+    PlantPlannedChangesComponent,
+    PlannedChangesFormComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AngularSlickgridModule.forRoot(),
+    AngularSlickgridModule.forRoot({
+      alwaysShowVerticalScroll: true,
+      autoEdit: false,
+      asyncEditorLoading: false,
+      autoFitColumnsOnFirstLoad: true,
+      autoResize: {
+        applyResizeToContainer: true,
+        calculateAvailableSizeBy: 'window',
+        bottomPadding: 20,
+        minHeight: 180,
+        minWidth: 300,
+        rightPadding: 0,
+      },
+      cellHighlightCssClass: 'slick-cell-modified',
+      checkboxSelector: {
+        cssClass: 'slick-cell-checkboxsel',
+      },
+      columnPicker: {
+        fadeSpeed: 0,
+        hideForceFitButton: false,
+        hideSyncResizeButton: true,
+        headerColumnValueExtractor: pickerHeaderColumnValueExtractor,
+      },
+      cellMenu: {
+        autoAdjustDrop: true,
+        autoAlignSide: true,
+        hideCloseButton: true,
+        hideCommandSection: false,
+        hideOptionSection: false,
+      },
+      contextMenu: {
+        autoAdjustDrop: true,
+        autoAlignSide: true,
+        hideCloseButton: true,
+        hideClearAllGrouping: false,
+        hideCollapseAllGroups: false,
+        hideCommandSection: false,
+        hideCopyCellValueCommand: false,
+        hideExpandAllGroups: false,
+        hideExportCsvCommand: false,
+        hideExportExcelCommand: false,
+        hideExportTextDelimitedCommand: true,
+        hideMenuOnScroll: true,
+        hideOptionSection: false,
+        iconCopyCellValueCommand: 'fa fa-clone',
+        iconExportCsvCommand: 'fa fa-download',
+        iconExportExcelCommand: 'fa fa-file-excel-o text-success',
+        iconExportTextDelimitedCommand: 'fa fa-download',
+      },
+      customFooterOptions: {
+        dateFormat: 'YYYY-MM-DD, hh:mm a',
+        hideRowSelectionCount: false,
+        hideTotalItemCount: false,
+        hideLastUpdateTimestamp: true,
+        footerHeight: 25,
+        leftContainerClass: 'col-xs-12 col-sm-5',
+        rightContainerClass: 'col-xs-6 col-sm-7',
+        metricSeparator: '|',
+        metricTexts: {
+          items: 'items',
+          itemsKey: 'ITEMS',
+          itemsSelected: 'items selected',
+          itemsSelectedKey: 'ITEMS_SELECTED',
+          of: 'of',
+          ofKey: 'OF',
+        },
+      },
+      dataView: {
+        syncGridSelection: true, // when enabled, this will preserve the row selection even after filtering/sorting/grouping
+        syncGridSelectionWithBackendService: false, // but disable it when using backend services
+      },
+      datasetIdPropertyName: 'id',
+      defaultFilter: Filters.input,
+      defaultBackendServiceFilterTypingDebounce: 500,
+      defaultColumnSortFieldId: 'id',
+      defaultFilterPlaceholder: '🔎︎',
+      defaultFilterRangeOperator: OperatorType.rangeInclusive,
+      editable: false,
+      enableAutoResize: true,
+      enableAutoSizeColumns: true,
+      enableCellNavigation: false,
+      enableColumnPicker: true,
+      enableColumnReorder: true,
+      enableColumnResizeOnDoubleClick: true,
+      enableContextMenu: true,
+      enableExcelExport: true, // Excel Export is the new default,
+
+      registerExternalResources: [new ExcelExportService()],
+
+      enableExport: false, // CSV/Text with Tab Delimited
+      enableFilterTrimWhiteSpace: false, // do we want to trim white spaces on all Filters?
+      enableGridMenu: true,
+      enableHeaderMenu: true,
+      enableEmptyDataWarningMessage: true,
+      emptyDataWarning: {
+        className: 'slick-empty-data-warning',
+        message: 'No data to display.',
+        messageKey: 'EMPTY_DATA_WARNING_MESSAGE',
+        hideFrozenLeftWarning: false,
+        hideFrozenRightWarning: false,
+        leftViewportMarginLeft: '40%',
+        rightViewportMarginLeft: '40%',
+        frozenLeftViewportMarginLeft: '0px',
+        frozenRightViewportMarginLeft: '40%',
+      },
+      enableMouseHoverHighlightRow: true,
+      enableSorting: true,
+      enableTextSelectionOnCells: true,
+      eventNamingStyle: EventNamingStyle.camelCase,
+      explicitInitialization: true,
+      excelExportOptions: {
+        addGroupIndentation: true,
+        exportWithFormatter: false,
+        filename: 'export',
+        format: FileType.xlsx,
+        groupingColumnHeaderTitle: 'Group By',
+        groupCollapsedSymbol: '⮞',
+        groupExpandedSymbol: '⮟',
+        groupingAggregatorRowText: '',
+        sanitizeDataExport: false,
+      },
+      exportOptions: {
+        delimiter: DelimiterType.comma,
+        exportWithFormatter: false,
+        filename: 'export',
+        format: FileType.csv,
+        groupingColumnHeaderTitle: 'Group By',
+        groupingAggregatorRowText: '',
+        sanitizeDataExport: false,
+        useUtf8WithBom: true,
+      },
+      filterTypingDebounce: 0,
+      forceFitColumns: false,
+      frozenHeaderWidthCalcDifferential: 0,
+      gridMenu: {
+        dropSide: 'left',
+        commandLabels: {
+          clearAllFiltersCommandKey: 'CLEAR_ALL_FILTERS',
+          clearAllSortingCommandKey: 'CLEAR_ALL_SORTING',
+          clearFrozenColumnsCommandKey: 'CLEAR_PINNING',
+          exportCsvCommandKey: 'EXPORT_TO_CSV',
+          exportExcelCommandKey: 'EXPORT_TO_EXCEL',
+          exportTextDelimitedCommandKey: 'EXPORT_TO_TAB_DELIMITED',
+          refreshDatasetCommandKey: 'REFRESH_DATASET',
+          toggleFilterCommandKey: 'TOGGLE_FILTER_ROW',
+          togglePreHeaderCommandKey: 'TOGGLE_PRE_HEADER_ROW',
+        },
+        hideClearAllFiltersCommand: false,
+        hideClearAllSortingCommand: false,
+        hideClearFrozenColumnsCommand: true, // opt-in command
+        hideExportCsvCommand: false,
+        hideExportExcelCommand: false,
+        hideExportTextDelimitedCommand: true,
+        hideForceFitButton: false,
+        hideRefreshDatasetCommand: false,
+        hideSyncResizeButton: true,
+        hideToggleFilterCommand: false,
+        hideTogglePreHeaderCommand: false,
+        iconCssClass: 'fa fa-bars',
+        iconClearAllFiltersCommand: 'fa fa-filter text-danger',
+        iconClearAllSortingCommand: 'fa fa-unsorted text-danger',
+        iconClearFrozenColumnsCommand: 'fa fa-times',
+        iconExportCsvCommand: 'fa fa-download',
+        iconExportExcelCommand: 'fa fa-file-excel-o text-success',
+        iconExportTextDelimitedCommand: 'fa fa-download',
+        iconRefreshDatasetCommand: 'fa fa-refresh',
+        iconToggleFilterCommand: 'fa fa-random',
+        iconTogglePreHeaderCommand: 'fa fa-random',
+        menuWidth: 16,
+        resizeOnShowHeaderRow: true,
+        headerColumnValueExtractor: pickerHeaderColumnValueExtractor,
+      },
+      headerMenu: {
+        autoAlign: true,
+        autoAlignOffset: 12,
+        minWidth: 140,
+        iconClearFilterCommand: 'fa fa-filter text-danger',
+        iconClearSortCommand: 'fa fa-unsorted',
+        iconFreezeColumns: 'fa fa-thumb-tack',
+        iconSortAscCommand: 'fa fa-sort-amount-asc',
+        iconSortDescCommand: 'fa fa-sort-amount-desc',
+        iconColumnHideCommand: 'fa fa-times',
+        iconColumnResizeByContentCommand: 'fa fa-arrows-h',
+        hideColumnResizeByContentCommand: false,
+        hideColumnHideCommand: false,
+        hideClearFilterCommand: false,
+        hideClearSortCommand: false,
+        hideFreezeColumnsCommand: true, // opt-in command
+        hideSortCommands: false,
+      },
+      headerRowHeight: 35,
+      multiColumnSort: true,
+      numberedMultiColumnSort: true,
+      tristateMultiColumnSort: false,
+      sortColNumberInSeparateSpan: true,
+      suppressActiveCellChangeOnEdit: true,
+      pagination: {
+        pageSizes: [10, 15, 20, 25, 30, 40, 50, 75, 100],
+        pageSize: 25,
+        totalItems: 0,
+      },
+      // technically speaking the Row Detail requires the process & viewComponent but we'll ignore it just to set certain options
+      rowDetailView: {
+        collapseAllOnSort: true,
+        cssClass: 'detail-view-toggle',
+        panelRows: 1,
+        keyPrefix: '__',
+        useRowClick: false,
+        useSimpleViewportCalc: true,
+        saveDetailViewOnScroll: false,
+      } as RowDetailView,
+      rowHeight: 35,
+      topPanelHeight: 35,
+      translationNamespaceSeparator: ':',
+      resetFilterSearchValueAfterOnBeforeCancellation: true,
+      resizeByContentOnlyOnFirstLoad: true,
+      resizeByContentOptions: {
+        alwaysRecalculateColumnWidth: false,
+        cellCharWidthInPx: 7.8,
+        cellPaddingWidthInPx: 14,
+        defaultRatioForStringType: 0.88,
+        formatterPaddingWidthInPx: 0,
+        maxItemToInspectCellContentWidth: 1000,
+        maxItemToInspectSingleColumnWidthByContent: 5000,
+        widthToRemoveFromExceededWidthReadjustment: 50,
+      },
+      treeDataOptions: ({
+        exportIndentMarginLeft: 5,
+        exportIndentationLeadingChar: '͏͏͏͏͏͏͏͏͏·',
+      } as unknown) as TreeDataOption,
+    }),
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -163,3 +404,10 @@ export function appInitializerFactory(
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
+function pickerHeaderColumnValueExtractor(column: Column) {
+  const headerGroup = (column && column.columnGroup) || ''
+  if (headerGroup) {
+    return headerGroup + ' - ' + column.name
+  }
+  return (column && column.name) || ''
+}
